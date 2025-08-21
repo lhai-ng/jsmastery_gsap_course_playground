1f136ba9521a2dd316f5a7e6a9442512561f61ee
Long Hai Nguyen <longhai14149@gmail.com>
2025-08-21 14:47:11 +0700

    Completed 11B

---
+++ pages/11b_challenge-from/script.js
@@ -0,0 +1,29 @@
+import gsap from "gsap";
+const card = document.querySelector('#card');
+
+gsap.from(card, {
+    y: 100,
+    opacity: 0,
+    duration: 1.2,
+    rotationX: -15,
+    ease: 'back.out(1.7)',
+    delay: .3
+})
+
+card.onmouseenter = () => {
+    gsap.to(card, {
+        rotationY: 180,
+        ease: 'power2.out',
+        duration: 0.8,
+        transformOrigin: 'center center',
+    })
+}
+
+card.onmouseleave = () => {
+    gsap.to(card, {
+        rotationY: 0,
+        ease: 'power2.out',
+        duration: 0.8,
+        transformOrigin: 'center center',
+    })
+}
